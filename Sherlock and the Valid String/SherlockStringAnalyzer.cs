84f98edf3f8d61abacc2196380145a21282540ab
Iluha <SvilsaX@gmail.com>
2020-08-22 02:11:02 +0300

    small refactor Sherlock and the Valid String

--- Sherlock and the Valid String/SherlockStringAnalyzer.cs
+++ Sherlock and the Valid String/SherlockStringAnalyzer.cs
@@ -6,7 +6,7 @@ namespace Sherlock_and_the_Valid_String
 {
     public static class SherlockStringAnalyzer
     {
-        public static string IsValid (string verifyingString)
+        public static string IsValid(string verifyingString)
         {
             switch (verifyingString.Length)
             {
@@ -18,19 +18,20 @@ namespace Sherlock_and_the_Valid_String
                 {
                     var charDatabase = verifyingString
                         .GroupBy(ch => ch)
-                        .ToDictionary(charGroup => charGroup.Key, charGroup => charGroup.Count());
+                        .Select(groupChar => groupChar.ToArray().Length)
+                        .OrderBy(charCount => charCount)
+                        .ToArray();
 
                     return TryValidate(charDatabase) ? "YES" : "NO";
                 }
             }
         }
 
-        private static bool TryValidate(Dictionary<char, int> charDatabase)
+        private static bool TryValidate(IReadOnlyList<int> sortedCharCount)
         {
-            var sortedCharCount = charDatabase.Values.OrderBy(x => x).ToArray();
             var tryCounter = 0;
             int smallestNum;
-            
+
             // If we see that the smallest number of certain character appears once in the sequence, 
             // skip it and use our attempt to delete one character
             if (sortedCharCount[1] != sortedCharCount[0])
@@ -39,13 +40,14 @@ namespace Sherlock_and_the_Valid_String
                 tryCounter++;
             }
             else smallestNum = sortedCharCount[0];
-            
-            for (var i = 1; i < sortedCharCount.Length; i++)
+
+            for (var i = 1; i < sortedCharCount.Count; i++)
             {
                 if (sortedCharCount[i] > smallestNum + 1) return false;
                 if (sortedCharCount[i] == smallestNum + 1) tryCounter++;
                 if (tryCounter == 2) return false;
             }
+
             return true;
         }
     }
