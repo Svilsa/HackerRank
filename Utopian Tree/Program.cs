48ad6b4933c22fe0245678dc910b678105f48939
Iluha <SvilsaX@gmail.com>
2020-11-23 17:33:53 +0300

    add the Utopian Tree solution

---
+++ Utopian Tree/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Utopian_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new UtopianTree(1);
+            Console.WriteLine(tree.PredictGrowth(5));
+        }
+    }
+}
---
+++ Utopian Tree/UtopianTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Utopian_Tree
+{
+    /// <summary>
+    /// This class represents a Utopian Tree
+    /// </summary>
+    /// <remarks>
+    /// This class predict Utopian Tree through seasons.
+    /// </remarks>
+    public class UtopianTree
+    {
+        private enum Seasons
+        {
+            Winter = 1,
+            Spring,
+            Summer,
+            Autumn
+        }
+
+        public int InitialHeight { get; }
+
+        public UtopianTree(int initialHeight)
+        {
+            InitialHeight = initialHeight;
+        }
+
+        /// <summary>
+        /// Predict the height of the tree through several seasons  
+        /// </summary>
+        /// <returns>
+        /// The height of the tree through specified number of seasons.
+        /// </returns>
+        /// <param name="numberOfSeasons">A specified number of seasons</param>
+        public int PredictGrowth(int numberOfSeasons)
+        {
+            var currentHeight = InitialHeight;
+            var currentSeason = Seasons.Spring;
+
+            for (var i = 0; i < numberOfSeasons; i++)
+            {
+                switch (currentSeason)
+                {
+                    case Seasons.Spring: currentHeight *= 2;
+                        currentSeason = Seasons.Summer;
+                        break;
+                    case Seasons.Summer: currentHeight += 1;
+                        currentSeason = Seasons.Spring;
+                        break;
+                }
+            }
+            return currentHeight;
+        }
+    }
+}
