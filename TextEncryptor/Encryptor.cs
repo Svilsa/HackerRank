5a635058a83061186ea6391df179946f1115929f
Iluha <SvilsaX@gmail.com>
2021-03-27 03:02:22 +0300

    add TextEncryptor

---
+++ TextEncryptor/Encryptor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextEncryptor
+{
+    public static class Encryptor
+    {
+        private static int EncryptionKey = 0b0011111001101011;
+        
+        public static string Encrypt(string text)
+        {
+            var encryptedString = new StringBuilder(text.Length);
+            
+            foreach (var c in text)
+            {
+                var encryptedChar = c ^ EncryptionKey;
+                encryptedString.Append(Convert.ToChar(encryptedChar));
+            }
+
+            return encryptedString.ToString();
+        }
+
+        public static string Decrypt(string text) => Encrypt(text);
+
+    }
+}
