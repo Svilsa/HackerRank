79ff78f329f25fb64412a692ca47ab03e42dfe06
Svilsa <SvilsaX@gmail.com>
2023-07-04 15:24:09 +0300

    add template for TextEncryption

---
+++ TextEncryption/src/TextEncryption/StringEncryptor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TextEncryption;
+
+public class StringEncryptor : StringEncryptorBase
+{
+    public override string Encrypt(string plainText, string passPhrase)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string Decrypt(string cipherText, string passPhrase)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ TextEncryption/src/TextEncryption/StringEncryptorBase.cs
@@ -0,0 +1,8 @@
+﻿namespace TextEncryption;
+
+public abstract class StringEncryptorBase
+{
+    public abstract string Encrypt(string plainText, string passPhrase);
+
+    public abstract string Decrypt(string cipherText, string passPhrase);
+}
